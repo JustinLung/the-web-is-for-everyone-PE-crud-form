787e245691aa66765fc9b213310c7cbd0d6e7f6b
JustinLung <justinlung77@gmail.com>
2022-04-05 11:06:25 +0200

    :sparkles: Enhanced CSS

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { getAllBooks } = require('./../modules/api')
 
 router.get('/', (req, res) => {
-  getAllBooks().then((data) => {
-    console.log(data)
-    res.render('index')
+  res.render('index', {
+    title: 'Scrollbook CRUD | Category',
   })
 })
 

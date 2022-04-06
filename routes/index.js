6c9b24fb8cf0049b65be5a442a4029505ee38ecb
JustinLung <justinlung77@gmail.com>
2022-04-06 16:39:10 +0200

    :construction: rendering on server

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,28 @@
 const express = require('express')
 const router = express.Router()
+const fetch = (...args) =>
+  import('node-fetch').then(({ default: fetch }) => fetch(...args))
 
-router.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Scrollbook CRUD | Category',
+router
+  .get('/', (req, res) => {
+    res.render('index', {
+      title: 'Scrollbook CRUD | Category',
+    })
   })
-})
+  .post('/', (req, res) => {
+    const type = req.body
+    const options = {
+      method: 'POST',
+      body: type,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+    fetch('https://scrollbook.api.fdnd.nl/v1/category', options)
+      .then((res) => res.json())
+      .catch((err) => {
+        console.log(err)
+      })
+  })
 
 module.exports = router

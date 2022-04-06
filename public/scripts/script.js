76041acb44e4ffa34405d2bae15cccffa99dfb28
JustinLung <justinlung77@gmail.com>
2022-04-06 11:08:47 +0200

    :sparkles: client side post function made

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,14 +1,21 @@
 var submitBtn = document.getElementById('submitButton')
+var input = document.getElementById('category')
 
 submitBtn.onclick = postData
 
-function onSubmit(e) {
-  if (!window.XMLHttpRequest) {
-    console.log('XHr is not supported')
-    return false
+function postData(e) {
+  // Send Data to the API
+  if (window.XMLHttpRequest) {
+    var category = JSON.stringify({ type: input.value })
+    var xhr = new XMLHttpRequest()
+    xhr.onreadystatechange = function (e) {
+      if (xhr.status === 200 && xhr.readyState === 4) {
+        var res = JSON.parse(xhr.responseText)
+        console.log(res)
+      }
+    }
+    xhr.open('POST', 'https://scrollbook.api.fdnd.nl/v1/category')
+    xhr.send(category)
+    e.preventDefault()
   }
-  xhr.open('POST', 'https://scrollbook.api.fdnd.nl/v1/category')
-  xhr.send()
-
-  e.preventDefault()
 }
